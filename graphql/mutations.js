6c5b635ae63c1b74f6c7205ba810766068d87490
silvertechsavvy <silvertechsavvy@gmail.com>
2022-07-19 18:25:43 -0300

    feature: login mutation

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -33,6 +33,31 @@ const register = {
   },
 };
 
+const login = {
+  type: GraphQLString,
+  args: {
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+  },
+  async resolve(_, args) {
+    const user = await User.findOne({ email: args.email }).select("+password");
+
+    console.log(user);
+
+    if (!user || args.password !== user.password)
+      throw new Error("Invalid Credentials");
+
+    const token = createJWT({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+
+    return token;
+  },
+};
+
 module.exports = {
   register,
+  login,
 };
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,7 +1,7 @@
 const { GraphQLSchema, GraphQLObjectType } = require("graphql");
 
 const { hello } = require("./queries");
-const { register } = require("./mutations");
+const { register, login } = require("./mutations");
 
 const QueryType = new GraphQLObjectType({
   name: "QueryType",
@@ -16,6 +16,7 @@ const MutationType = new GraphQLObjectType({
   description: "The root mutation type",
   fields: {
     register,
+    login,
   },
 });
 

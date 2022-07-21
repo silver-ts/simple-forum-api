3bef9c9b570265294be3bcb4afd9bd393a812798
silvertechsavvy <silvertechsavvy@gmail.com>
2022-07-21 00:35:02 -0300

    feature: adjusting for heroku deploy

--- server.js
+++ server.js
@@ -24,5 +24,7 @@ app.use(
   })
 );
 
-app.listen(3000);
-console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
+console.log(`Server is running on port ${PORT}`);
